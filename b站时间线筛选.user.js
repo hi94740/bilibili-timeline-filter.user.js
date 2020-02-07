4d041f245098c06c6a94cc74341e5bc6c39f0d59
hi94740 <hi94740@qq.com>
2020-02-07 21:34:11 +0800

    optimization and code style improvements

--- b站时间线筛选.user.js
+++ b站时间线筛选.user.js
@@ -17,7 +17,8 @@
 
 var $ = unsafeWindow.jQuery
 
-GM.getResourceUrl("css").then(u => $("head").append('<link rel="stylesheet" href="' + u + '">'))
+GM.getResourceUrl("css")
+  .then(u => $("head").append('<link rel="stylesheet" href="' + u + '">'))
 Vue.use(vant.Tab)
 Vue.use(vant.Tabs)
 
@@ -38,9 +39,9 @@ const filterDynamicWithTag = function(selection) {
     new Promise(res => {setInterval(function() {
       if ($(".card").length > 0) res()
     })}).then(function() {
-      cardObserver.observe($(".card").parent()[0],{childList:true,subtree:true})
       clearFilters()
       filterWorker()
+      cardObserver.observe($(".card").parent()[0],{childList:true,subtree:true})
     })
   }
 }
@@ -115,16 +116,22 @@ function ajaxWithCredentials(url) {
 }
 
 function fetchTags(requestWithCredentials) {
-  return requestWithCredentials("https://api.live.bilibili.com/User/getUserInfo").then(data => {
+  return requestWithCredentials("https://api.live.bilibili.com/User/getUserInfo")
+    .then(data => {
       let uid = data.data.uid
       console.log("uid: " + uid)
-      return Promise.all([requestWithCredentials("https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=1&ps=50").then(data => {
-        let gf = range(2,Math.ceil(data.data.total/50),1).map(i => {
-          return requestWithCredentials("https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=" + i + "&ps=50")
-        })
-        gf.unshift(data)
-        return Promise.all(gf)
-      }),requestWithCredentials("https://api.bilibili.com/x/relation/tags?vmid=" + uid)])
+      return Promise.all([
+          requestWithCredentials("https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=1&ps=50")
+            .then(data => {
+              let gf = range(2,Math.ceil(data.data.total/50),1)
+                .map(i => {
+                  return requestWithCredentials("https://api.bilibili.com/x/relation/followings?vmid=" + uid + "&pn=" + i + "&ps=50")
+                })
+              gf.unshift(data)
+              return Promise.all(gf)
+            }),
+        requestWithCredentials("https://api.bilibili.com/x/relation/tags?vmid=" + uid)
+      ])
     }).then(data => {
       let followings = data[0].map(p => p.data.list).flat()
       let tags = data[1].data
@@ -146,7 +153,8 @@ function fetchTags(requestWithCredentials) {
 
 
 
-Promise.all([new Promise(res => {
+Promise.all([
+  new Promise(res => {
     setInterval(function () {
       if ($(".tab-bar").length == 1) res()
     })
@@ -155,7 +163,9 @@ Promise.all([new Promise(res => {
     $("#filterUI")[0].hidden = true
     autoPadding()
     tabObserver.observe($(".tab-bar")[0],{childList:true,subtree:true,attributes:true})
-  }),fetchTags(ajaxWithCredentials)]).then(data => {
+  }),
+  fetchTags(ajaxWithCredentials)
+]).then(data => {
   tagged = data[1]
   console.log(tagged)
   let tagOptions = tagged.filter(t => t.count != 0)
@@ -173,5 +183,5 @@ Promise.all([new Promise(res => {
   isBangumiTimeline()
 }).catch(err => {
   console.error(err)
-  alert("b站时间线筛选脚本出错了！\n请查看控制台以获取错误信息")
+  alert("【b站时间线筛选】脚本出错了！\n请查看控制台以获取错误信息")
 })
